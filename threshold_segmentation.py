94c83ebc01e7328794e782201ab4af20ec1c7dcf
bashmac.wsl <vasabi.ph@gmail.com>
2025-12-17 14:05:04 +0300

    Remove nvidia header from threshold_segmentation. This file was copied and modified from the official repo of RADIO model

--- threshold_segmentation.py
+++ threshold_segmentation.py
@@ -1,10 +1,4 @@
-# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
-#
-# NVIDIA CORPORATION and its licensors retain all intellectual property
-# and proprietary rights in and to this software, related documentation
-# and any modifications thereto.  Any use, reproduction, disclosure or
-# distribution of this software and related documentation without an express
-# license agreement from NVIDIA CORPORATION is strictly prohibited.
+# copied and modified from https://github.com/NVlabs/RADIO/blob/main/examples/visualize_video_features.py
 import argparse
 from collections import defaultdict
 from functools import partial
